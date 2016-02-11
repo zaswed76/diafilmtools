17e9cf9d136fd641171180a84db1e27d4d90003e
serg <zaswed76@gmail.com>
2016-02-11 06:11:45 +0200

    111

---
+++ setup.py
@@ -0,0 +1,22 @@
+from os.path import join, dirname
+
+import diatools
+from setuptools import setup, find_packages
+
+setup(
+        name="program",
+        # в __init__ пакета
+        version=diatools.__version__,
+        packages=find_packages(
+                exclude=["*.exemple", "*.exemple.*", "exemple.*",
+                         "exemple"]),
+        include_package_data=True,
+        long_description=open(
+                join(dirname(__file__), 'README.rst')).read(),
+        install_requires=[],
+        entry_points={
+            'console_scripts':
+                ['diatools-min = diatools.dia_min:diatools_min']
+        }
+
+)

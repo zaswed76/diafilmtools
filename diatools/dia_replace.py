7b4c2fd4e9aad16cae00385d74fe8b0248b5020f
serg <zaswed76@gmail.com>
2016-02-18 13:26:38 +0200

    add dia_replace

--- diatools/dia_replace.py
+++ diatools/dia_replace.py
@@ -5,14 +5,13 @@ import argparse
 import os
 import sys
 
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtCore import pyqtSignal
+from PyQt5 import QtWidgets
 
 from diatools import files_tool
+from diatools import min_ggrid
 
 SIZE_IMAGE = 250
 MARGIN = 20
-
 
 
 def arg_parser():
@@ -25,7 +24,7 @@ def arg_parser():
     return parser
 
 
-def get_diafilm_dir(miniature_path, ):
+def get_diafilm_data(miniature_path):
     dir_name, dia_name = os.path.split(
             os.path.splitext(miniature_path)[0])
     config_name = files_tool.DATA_FILE_NAME
@@ -34,101 +33,25 @@ def get_diafilm_dir(miniature_path, ):
     data = conf_obj.load()
     diafilm = data[dia_name]['dia_dir']
     size = data[dia_name]['size']
+    resample = data[dia_name]['resample']
+
+
     files = files_tool.collect_files_by_extensions(diafilm,
                                                    files_tool.IMAGEEXTS,
                                                    sort=True,
                                                    reverse=False)
-    return files
-
-
-class Cell(QtWidgets.QLabel):
-    click = pyqtSignal(str)
-
-    def __init__(self, size, path_image=None):
-        super().__init__()
-        self.path_image = path_image
-        if path_image is not None:
-            pixmap = QtGui.QPixmap(path_image).scaled(size,
-                                                      size,
-                                                      1, 1)
-            self.setPixmap(pixmap)
-        else:
-            self.setFixedWidth(size)
-            self.setFixedHeight(size)
-            self.setStyleSheet("background-color: lightgrey")
-
-    def mousePressEvent(self, QMouseEvent):
-        self.click.emit(self.path_image)
-
-
-class DisplayGrid(QtWidgets.QLabel):
-    def __init__(self, *__args):
-        super().__init__(*__args)
-
-        self.grid = QtWidgets.QGridLayout(self)
-        self.grid.setSpacing(0)
-        self.grid.setContentsMargins(0, 0, 0, 0)
-
-
-
-    def create_grid(self, img_lst, column, img_size):
-        lines = int(len(img_lst) / column + 1)
-        width = column * img_size + ((column -1) * MARGIN)
-        print(column, img_size, width)
-        height = lines * img_size
-
-        n = 0
-        for x in range(lines):
-            for y in range(column):
-                try:
-                    img = img_lst[n]
-                    n += 1
-                except IndexError:
-                    img = None
-                label = Cell(img_size, img)
-                # label.click.connect(self.change_image)
-                self.grid.addWidget(label, x, y)
-        return width, height
-
-
-class Widget(QtWidgets.QFrame):
-    def __init__(self):
-        super().__init__()
-        box = QtWidgets.QVBoxLayout(self)
-        box.setContentsMargins(0, 0, 0, 0)
-        box.setSpacing(0)
-        self.display = DisplayGrid()
-        self.scroll = QtWidgets.QScrollArea()
-        self.scroll.setWidgetResizable(True)
-        box.addWidget(self.scroll)
-        self.scroll.setWidget(self.display)
-
-
-
-
-
-
-    def create_grid(self, lst, column, size):
-        width, height = self.display.create_grid(lst, column, size)
-        self.display.setFixedSize(width, height)
-        print(width)
-        self.setFixedWidth(width + 100)
-        self.setMinimumHeight(600)
-
-    def change_image(self, path):
-        # create_miniature(path, 240)
-        self.close()
+    return files, size, resample
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    wind = Widget()
     parser = arg_parser()
     arg = parser.parse_args()
     path = arg.miniature
-    text = get_diafilm_dir(path)
-    wind.create_grid(text, 3, 200)
+    files, size, resample = get_diafilm_data(path)
+    wind = min_ggrid.Widget(files, path, size, resample)
     wind.show()
+    wind.setMinimumWidth(wind.recommended_size)
     sys.exit(app.exec_())
 
 
---
+++ diatools/min_ggrid.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import math
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import pyqtSignal
+
+from diatools import image_tool
+
+SPACING = 30
+DEFAULT_COLUMN = 4
+SIZE_IMAGE = 250
+
+def get_number_of_lines(leng, col):
+    return math.ceil(leng / col)
+
+
+class Cell(QtWidgets.QLabel):
+    click = pyqtSignal(str)
+
+    def __init__(self, path=None, *__args):
+        super().__init__(*__args)
+        self.path = path
+        self.setAlignment(QtCore.Qt.AlignCenter)
+        if self.path:
+            self.set_image()
+
+    def mousePressEvent(self, QMouseEvent):
+        self.click.emit(self.path)
+
+    def set_image(self):
+        self.setPixmap(QtGui.QPixmap(self.path).scaled(SIZE_IMAGE, SIZE_IMAGE, 1, 1))
+
+
+class Grid(QtWidgets.QFrame):
+    def __init__(self, parent, file_list, columns, lines, spacing):
+        super().__init__()
+        self.parent = parent
+        self.file_list = file_list
+        self.columns = columns
+        self.lines = lines
+        box = QtWidgets.QVBoxLayout(self)
+        box.setContentsMargins(20, 20, 20, 20)
+        box.setSpacing(0)
+
+        self.grid = QtWidgets.QGridLayout()
+        self.grid.setContentsMargins(0, 0, 0, 0)
+        self.grid.setSpacing(spacing)
+
+        box.addLayout(self.grid)
+        self.create_grid()
+
+    def create_grid(self):
+        n = 0
+        cell = dict()
+        for x in range(self.lines):
+            for y in range(self.columns):
+                try:
+                    cell[(x, y)] = Cell(self.file_list[n])
+                except IndexError:
+                    cell[(x, y)] = Cell()
+                cell[(x, y)].click.connect(self.parent.change_image)
+                n += 1
+                self.grid.addWidget(cell[(x, y)], x, y)
+
+
+class Widget(QtWidgets.QFrame):
+    def __init__(self, lst, target, size_miniature, resample):
+        super().__init__()
+        self.target = target
+        self.resample = resample
+        self.size_miniature = size_miniature
+        self.file_lst = lst
+        box = self.box(0, (0, 0, 0, 0), self)
+
+        self.scroll = QtWidgets.QScrollArea()
+        self.scroll.setWidgetResizable(True)
+        box.addWidget(self.scroll)
+        lines = get_number_of_lines(len(self.file_lst), DEFAULT_COLUMN)
+        self.update_grid(self.file_lst, DEFAULT_COLUMN, lines, SPACING)
+
+    def update_grid(self, lst, columns, lines, spacing):
+        self.grid = Grid(self, lst, columns, lines, spacing)
+        self.scroll.setWidget(self.grid)
+
+    @property
+    def recommended_size(self):
+        return self.grid.width() + SPACING
+
+    def box(self, spacin, margin, parent):
+        box = QtWidgets.QVBoxLayout(parent)
+        box.setContentsMargins(*margin)
+        box.setSpacing(spacin)
+        return box
+
+    def change_image(self, path):
+        image_tool.thumbnail(path, self.target, self.size_miniature,
+                             self.resample)
+        self.close()
+
+
+if __name__ == '__main__':
+    pass
